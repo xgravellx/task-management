ecd79aa2418c8d684b2d803e5c3d8de6f77253a5
Hazal Yilmaz <hazal.yilmaz@mahrek.com.tr>
2023-09-09 17:19:41 +0300

    Initial commit

---
+++ task-management-app/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { Styled } from "components/Sidebar/Sidebar.styled.js";
+
+const Sidebar = () => {
+    return (
+        <Styled>
+            Sidebar
+        </Styled>
+    );
+};
+
+export default Sidebar;
